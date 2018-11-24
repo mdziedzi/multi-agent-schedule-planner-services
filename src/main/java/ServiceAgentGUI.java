d3c0630d3d8f8cda37f0031274ef9fd4a48283c9
mdziedzi <marcindziedzic8@gmail.com>
2018-11-24 19:15:34 +0100

    Add ServiceAgent and ServiceAgentGUI example.

---
+++ src/main/java/ServiceAgentGUI.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class ServiceAgentGUI extends JFrame {
+
+    private final ServiceAgent serviceAgent;
+    private final JPanel jPanel;
+    private final JLabel helloLabel;
+
+    public ServiceAgentGUI(ServiceAgent serviceAgent) {
+        this.serviceAgent = serviceAgent;
+
+        setSize(200, 200);
+
+        jPanel = new JPanel();
+
+        helloLabel = new JLabel("Hello");
+
+
+        jPanel.add(helloLabel);
+
+
+        jPanel.setBackground(new Color(58, 121, 123));
+
+        this.add(jPanel);
+
+        setTitle(serviceAgent.getLocalName());
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        setVisible(true);
+    }
+}
